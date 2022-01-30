9b1db1d521d78bcee5ef732769e162362e48c982
Mendes Kevin <papawa.dev@gmail.com>
2022-01-31 00:28:59 +0100

    prefix ft_

--- src/math/inter.c
+++ src/math/inter.c
@@ -10,7 +10,7 @@ double ft_invlerp(double start, double end, double value)
 	return (value - start) / (end - start);
 }
 
-double remap(double input_a, double input_b, double out_a, double out_b, double value)
+double ft_remap(double input_a, double input_b, double out_a, double out_b, double value)
 {
 	return ft_lerp(out_a, out_b, ft_invlerp(input_a, input_b, value));
 }
