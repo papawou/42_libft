3f0a9f98f4d3d3385406b8c06a5921d53f5f643f
Mendosita <papawa.dev@gmail.com>
2022-08-18 14:10:22 +0200

    norm

--- src/math/utils_2.c
+++ src/math/utils_2.c
@@ -6,16 +6,11 @@
 /*   By: kmendes <kmendes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 03:29:38 by kmendes           #+#    #+#             */
-/*   Updated: 2022/05/31 03:32:42 by kmendes          ###   ########.fr       */
+/*   Updated: 2022/08/18 14:09:57 by kmendes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/math/ft_utils.h"
-
-double	ft_remap(t_remap_p p, double value)
-{
-	return (ft_lerp(p.out_a, p.out_b, ft_invlerp(p.input_a, p.input_b, value)));
-}
 
 float	deg_to_rad(float deg)
 {
