fd73a2fed2a5029bf8ad51bdf77e4a64bb2e23ec
Mendosita <papawa.dev@gmail.com>
2022-08-18 14:09:38 +0200

    move ft_remap to appropriate file

--- src/math/utils.c
+++ src/math/utils.c
@@ -6,7 +6,7 @@
 /*   By: kmendes <kmendes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 03:28:21 by kmendes           #+#    #+#             */
-/*   Updated: 2022/05/31 03:32:26 by kmendes          ###   ########.fr       */
+/*   Updated: 2022/08/18 14:09:02 by kmendes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,9 @@ double	ft_lerp(double start, double end, double alpha)
 double	ft_invlerp(double start, double end, double value)
 {
 	return ((value - start) / (end - start));
+}
+
+double	ft_remap(t_remap_p p, double value)
+{
+	return (ft_lerp(p.out_a, p.out_b, ft_invlerp(p.input_a, p.input_b, value)));
 }
