e69a2137415439e5a4c712b29dbc0a36a44ed90c
Mendosita <papawa.dev@gmail.com>
2022-08-29 13:49:15 +0200

    delete proto

--- inc/libft/math/ft_utils.h
+++ inc/libft/math/ft_utils.h
@@ -3,34 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_utils.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmendes <kmendes@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kmendes <kmendes@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 03:20:12 by kmendes           #+#    #+#             */
-/*   Updated: 2022/06/01 13:11:54 by kmendes          ###   ########.fr       */
+/*   Updated: 2022/08/29 13:48:02 by kmendes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_UTILS_H
 # define FT_UTILS_H
 
-# define PI 3.14159265358979323846
-
-typedef struct s_remap_p
-{
-	double	input_a;
-	double	input_b;
-	double	out_a;
-	double	out_b;
-}	t_remap_p;
-
-double		ft_clamp(double min, double max, double value);
-double		ft_lerp(double start, double end, double alpha);
-double		ft_invlerp(double start, double end, double value);
-double		ft_remap(t_remap_p p, double value);
-float		deg_to_rad(float deg);
-float		rad_to_deg(float rad);
-double		ft_fabs(double a);
-int			ft_abs(int a);
 int			ft_compswap(int *a, int *b);
 int			ft_fcompswap(float *a, float *b);
 int			ft_dcompswap(double *a, double *b);
